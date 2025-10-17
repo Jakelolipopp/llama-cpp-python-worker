2a6750c1a0d620a3024fa2862af5d5149d0af075
Vincent <schuhmacher@jaimo-solutions.com>
2025-10-17 10:54:37 +0200

    first real attempt at getting the llm stuff working

--- rp_handler.py
+++ rp_handler.py
@@ -26,9 +26,10 @@ else:
 
 
 if found_gguf:
+    print(f"Found gguf model at {gguf_path}, loading...")
     llm = Llama(
         model_path=gguf_path,
-        n_ctx=8192,      # The context size for the model.
+        n_ctx=16384,      # The context size for the model.
         n_gpu_layers=-1, # Offload all layers to the GPU. Set to 0 for CPU-only.
         verbose=True,    # Enable verbose logging from llama.cpp.
     )
@@ -37,21 +38,39 @@ def handler(event):
     global found_nas, found_gguf, llm
     print(f"Worker Start")
     if not found_nas:
-        return "No NAS found on startup"
+        yield "No NAS found on startup"
+        return
     if not found_gguf:
-        return "No gguf model found on startup"
+        yield "No gguf model found on startup"
+        return # I think both yields could be just returns but lets be safe
     input = event['input']
 
     prompt = input.get('prompt')  
     seconds = input.get('seconds', 0)  
 
     print(f"Received prompt: {prompt}")
+    messages = [
+        {
+            "role": "user",
+            "content": prompt
+        }
+    ]
+
+    stream = llm.create_chat_completion(
+        messages=messages,
+        stream=True
+    )
+    for chunk in stream:
+        content = chunk["choices"][0].get("delta", {}).get("content")
+        if content is not None:
+            yield content
+
     print(f"Sleeping for {seconds} seconds...")
     
     # Replace the sleep code with your Python function to generate images, text, or run any machine learning workload
     time.sleep(seconds)  
     
-    return prompt
+    return
 
 if __name__ == '__main__':
     runpod.serverless.start({'handler': handler })
